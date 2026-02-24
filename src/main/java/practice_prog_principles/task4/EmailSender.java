bbfdcc4e20e071b101edf64849b371c1c82ff773
KateLihomanova <kate.lihomanova@gmail.com>
2026-02-24 15:09:45 +0700

    Home task_prog_princ fix

--- src/main/java/practice_prog_principles/task4/EmailSender.java
+++ src/main/java/practice_prog_principles/task4/EmailSender.java
@@ -1,7 +1,7 @@
 package practice_prog_principles.task4;
 
 public class EmailSender {
-    public void processOrder() {
+    public void sendEmailConfirmation() {
         System.out.println("Обрабатываем заказ...");
     }
 }
